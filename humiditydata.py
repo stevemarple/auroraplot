f17b86cfa6bad877675ff715ae26fabeda94daec
Steve Marple <stevemarple@googlemail.com>
2013-09-26 12:24:40 +0100

    Adding class to support humidity sensors.

---
+++ humiditydata.py
@@ -0,0 +1,36 @@
+import numpy as np
+
+from auroraplot.data import Data
+
+class HumidityData(Data):
+    '''Class to manipulate and display humidity data.'''
+    def __init__(self,
+                 network=None,
+                 site=None,
+                 channels=None,
+                 start_time=None,
+                 end_time=None,
+                 sample_start_time=np.array([]),
+                 sample_end_time=np.array([]),
+                 integration_interval=np.array([]),
+                 nominal_cadence=None,
+                 data=np.array([]),
+                 units=None,
+                 sort=True):
+        Data.__init__(self,
+                      network=network,
+                      site=site,
+                      channels=channels,
+                      start_time=start_time,
+                      end_time=end_time,
+                      sample_start_time=sample_start_time,
+                      sample_end_time=sample_end_time,
+                      integration_interval=integration_interval,
+                      nominal_cadence=nominal_cadence,
+                      data=data,
+                      units=units,
+                      sort=sort)
+
+    def data_description(self):
+        return 'Humidity'
+
