11968fcfe1d889994bc54c5a9bf57b970bb61e22
Steve Marple <stevemarple@googlemail.com>
2014-12-09 23:09:12 +0000

    Adding estimated K-index scale for LAN2.

--- auroraplot/datasets/samnet.py
+++ auroraplot/datasets/samnet.py
@@ -352,6 +352,7 @@ sites = {
         'elevation': np.nan,
         'start_time': np.datetime64('2014-12-02T00:00:00+0000'),
         'end_time': None,
+        'k_index_scale': 650e-9, # Estimated
         'copyright': 'Lancaster University',
         'license': 'Data users are not entitled to distribute data to third parties outside their own research teams without requesting permission from Prof. F. Honary. Similarly, SAMNET data should not become part of a distributed database without permission first being sought. Commerical use prohibited.',
         'attribution': 'The Sub-Auroral Magnetometer Network data (SAMNET) is operated by the Space Plasma Environment and Radio Science (SPEARS) group, Department of Physics, Lancaster University.',
