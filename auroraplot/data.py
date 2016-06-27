fixed dt64._aggregate, so dt64.mean, etc, return with largest correct time units

auroraplot/data.py
@@ -647,6 +647,7 @@ class Data(object):
              units_prefix=None, title=None, 
              # Our own options
              start_time=None, end_time=None, time_units=None, add_legend=None,
+             samples_as_points=True,
              **kwargs):
 
         def bracket_units(units):
@@ -738,8 +739,11 @@ class Data(object):
             else:
                 ydata = self.data[cidx] / u['mul']
 
-            xdata = np.vstack((self.sample_start_time,self.sample_end_time)).flatten('F')
-            ydata = np.vstack((ydata,ydata)).flatten('F')
+            if samples_as_points:
+                xdata = dt64.mean(self.sample_start_time,self.sample_end_time)
+            else:
+                xdata = np.vstack((self.sample_start_time,self.sample_end_time)).flatten('F')
+                ydata = np.vstack((ydata,ydata)).flatten('F')
                 
             if 'label' in kwargs:
                 r.append(dt64.plot_dt64(xdata, ydata,
@@ -799,7 +803,7 @@ class Data(object):
         
     def set_cadence(self, cadence, ignore_nan=True,
                     offset_interval=None, inplace=False, fast=True,
-                    aggregate=None):
+                    aggregate=None,use_fallback=False):
         tu = dt64.get_units(cadence)
 
         if offset_interval is None:
@@ -807,8 +811,116 @@ class Data(object):
                 np.timedelta64(0, tu)
         if aggregate is None:
             aggregate=scipy.average
+            mean_integration = True # Otherwise sum
+        else:
+            use_fallback = True # aggregate not supported by fast method
 
-        if cadence > self.nominal_cadence and not fast:
+
+        import os
+        tsint_so_file = os.path.dirname(__file__) + '/tsintegrate.so'
+
+        try:
+            from numpy.ctypeslib import ndpointer
+            import ctypes
+            ctsint = ctypes.cdll.LoadLibrary(tsint_so_file)
+        except:
+            use_fallback = True
+            logger.info('Fast integration library (%s) unavailable, using fallback',
+                        tsint_so_file)
+            
+
+        if not use_fallback:
+            class OutStruct(ctypes.Structure):
+                _fields_ = [("data", ctypes.POINTER(ctypes.c_double)),
+                            ("weight", ctypes.POINTER(ctypes.c_double))]
+
+            sam_st = dt64.astype(np.arange(dt64.ceil(self.start_time, cadence) 
+                                           + offset_interval, 
+                                           self.end_time, 
+                                           cadence),
+                                 time_type=self.start_time,
+                                 units=tu)
+            sam_et = sam_st + cadence
+
+            nnew = sam_st.size
+            nold = self.sample_start_time.size
+            nchan = len(self.channels)
+            
+            if self.integration_interval is not None:
+                assert not np.any(dt64.isnat(self.integration_interval)), \
+                    'integration_interval must not contain NaT'
+            else:
+                self.integration_interval = np.ones([nchan,nold])\
+                                            *self.nominal_cadence
+
+            wu = dt64.get_units(self.integration_interval)
+            iu = dt64.smallest_unit((cadence,self.nominal_cadence,
+                                     self.sample_start_time,
+                                     self.sample_end_time,
+                                     sam_st,sam_et))
+            ius = 'M8['+iu+']'
+            
+            output_p = ctypes.POINTER(OutStruct)
+            ctsint.tsIntegrate.restype= ctypes.POINTER(OutStruct)
+            ctsint.tsIntegrate.argtypes = [ctypes.c_int64,
+                                ndpointer(dtype=ctypes.c_int64,shape=(nold,)),
+                                ndpointer(dtype=ctypes.c_int64,shape=(nold,)),
+                                ndpointer(dtype=ctypes.c_double,shape=(nold,)),
+                                ndpointer(dtype=ctypes.c_double,shape=(nold,)),
+                                ctypes.c_int64,
+                                ndpointer(dtype=ctypes.c_int64,shape=(nnew,)),
+                                ndpointer(dtype=ctypes.c_int64,shape=(nnew,))]
+            
+            new_data = np.zeros([nchan,nnew])
+            new_weight = np.zeros([nchan,nnew])
+            
+            self.integration_interval[np.isnan(self.data)] = 0
+            self.data[np.isnan(self.data)] = 0
+
+            for cn in range(nchan):
+                output_p = ctsint.tsIntegrate(np.int64(nold),
+                      self.sample_start_time.astype(ius).astype('int64'),
+                      self.sample_end_time.astype(ius).astype('int64'),
+                      self.data[cn,:].astype('float64'),
+                      self.integration_interval[cn,:].\
+                                         astype('int64').astype('float64'),
+                      np.int64(nnew),
+                      sam_st.astype(ius).astype('int64'),
+                      sam_et.astype(ius).astype('int64'))
+                new_weight[cn,:] = np.fromiter(output_p.contents.weight,
+                                            dtype=np.float64,count=nnew).copy()
+                new_data[cn,:] = np.fromiter(output_p.contents.data,
+                                             dtype=np.float64,count=nnew).copy()
+                ctsint.freemem()
+
+                if mean_integration:
+                    non_zero_weight = (new_weight[cn,:]!=0.0)
+                    new_data[cn,non_zero_weight] = new_data[cn,non_zero_weight]\
+                                                /new_weight[cn,non_zero_weight]
+            new_data[new_weight==0.0] = np.NaN
+
+            if inplace:
+                r = self
+            else:
+                r = copy.copy(self)
+                for k in (set(self.__dict__.keys())
+                          - set(['sample_start_time', 'sample_end_time', 
+                                 'integration_interval', 'nominal_cadence',
+                                 'data'])):
+                    setattr(r, k, copy.deepcopy(getattr(self, k)))
+
+            r.start_time = dt64.astype(r.start_time, units=tu)
+            r.end_time = dt64.astype(r.end_time, units=tu)
+            r.sample_start_time = sam_st
+            r.sample_end_time = sam_et
+            r.integration_interval = dt64.astype(new_weight,
+                                                 time_type=cadence,
+                                                 units=wu)
+            r.nominal_cadence = cadence.copy()
+            r.data = new_data
+
+            
+        elif cadence > self.nominal_cadence:
             sam_st = dt64.astype(np.arange(dt64.ceil(self.start_time, cadence) 
                                            + offset_interval, 
                                            self.end_time, 
@@ -884,96 +996,6 @@ class Data(object):
             r.nominal_cadence = cadence.copy()
             r.data = d
 
-        elif fast and cadence != self.nominal_cadence:
-            mean_integration = True # Otherwise sum
-            from numpy.ctypeslib import ndpointer
-            import ctypes
-            import os
-            this_dir = os.path.dirname(__file__)
-            ctsint = ctypes.cdll.LoadLibrary(this_dir+'/tsintegrate.so')
-            class OutStruct(ctypes.Structure):
-                _fields_ = [("data", ctypes.POINTER(ctypes.c_double)),
-                            ("weight", ctypes.POINTER(ctypes.c_double))]
-
-            sam_st = dt64.astype(np.arange(dt64.ceil(self.start_time, cadence) 
-                                           + offset_interval, 
-                                           self.end_time, 
-                                           cadence),
-                                 time_type=self.start_time,
-                                 units=tu)
-            sam_et = sam_st + cadence
-
-            nnew = sam_st.size
-            nold = self.sample_start_time.size
-            nchan = len(self.channels)
-            
-            if self.integration_interval is not None:
-                assert not np.any(dt64.isnat(self.integration_interval)), \
-                    'integration_interval must not contain NaT'
-            else:
-                self.integration_interval = np.ones([nchan,nold])\
-                                            *self.nominal_cadence
-
-            wu = dt64.get_units(self.integration_interval)
-            
-            output_p = ctypes.POINTER(OutStruct)
-            ctsint.tsIntegrate.restype= ctypes.POINTER(OutStruct)
-            ctsint.tsIntegrate.argtypes = [ctypes.c_int64,
-                                ndpointer(dtype=ctypes.c_int64,shape=(nold,)),
-                                ndpointer(dtype=ctypes.c_int64,shape=(nold,)),
-                                ndpointer(dtype=ctypes.c_double,shape=(nold,)),
-                                ndpointer(dtype=ctypes.c_double,shape=(nold,)),
-                                ctypes.c_int64,
-                                ndpointer(dtype=ctypes.c_int64,shape=(nnew,)),
-                                ndpointer(dtype=ctypes.c_int64,shape=(nnew,))]
-            
-            new_data = np.zeros([nchan,nnew])
-            new_weight = np.zeros([nchan,nnew])
-            
-            self.integration_interval[np.isnan(self.data)] = 0
-            self.data[np.isnan(self.data)] = 0
-
-            for cn in range(nchan):
-                output_p = ctsint.tsIntegrate(np.int64(nold),
-                      self.sample_start_time.astype('M8[ms]').astype('int64'),
-                      self.sample_end_time.astype('M8[ms]').astype('int64'),
-                      self.data[cn,:].astype('float64'),
-                      self.integration_interval[cn,:].\
-                                         astype('int64').astype('float64'),
-                      np.int64(nnew),
-                      sam_st.astype('M8[ms]').astype('int64'),
-                      sam_et.astype('M8[ms]').astype('int64'))
-                new_weight[cn,:] = np.fromiter(output_p.contents.weight,
-                                            dtype=np.float64,count=nnew).copy()
-                new_data[cn,:] = np.fromiter(output_p.contents.data,
-                                             dtype=np.float64,count=nnew).copy()
-                ctsint.freemem()
-
-                if mean_integration:
-                    non_zero_weight = (new_weight[cn,:]!=0.0)
-                    new_data[cn,non_zero_weight] = new_data[cn,non_zero_weight]\
-                                                /new_weight[cn,non_zero_weight]
-            new_data[new_weight==0.0] = np.NaN
-
-            if inplace:
-                r = self
-            else:
-                r = copy.copy(self)
-                for k in (set(self.__dict__.keys())
-                          - set(['sample_start_time', 'sample_end_time', 
-                                 'integration_interval', 'nominal_cadence',
-                                 'data'])):
-                    setattr(r, k, copy.deepcopy(getattr(self, k)))
-
-            r.start_time = dt64.astype(r.start_time, units=tu)
-            r.end_time = dt64.astype(r.end_time, units=tu)
-            r.sample_start_time = sam_st
-            r.sample_end_time = sam_et
-            r.integration_interval = dt64.astype(new_weight,
-                                                 time_type=cadence,
-                                                 units=wu)
-            r.nominal_cadence = cadence.copy()
-            r.data = new_data
         elif cadence < self.nominal_cadence:
             raise Exception('Interpolation to reduce cadence not implemented')
         else:

auroraplot/dt64tools.py
@@ -280,7 +280,12 @@ def _aggregate(*a, **kwargs):
             assert isinstance(b.dtype.type(), a0.dtype.type), \
                 'Arrays must hold the same data type'
             tmp += b.astype(d).astype('int64')
-        return (tmp / len(a)).astype(d)
+        for new_tu in time_units[time_units.index(tu)::-1]:
+            new_d = get_time_type(a0) + '[' + new_tu + ']'
+            ratio = np.array([1]).astype(d).astype(new_d).astype('int64')
+            if all( (tmp*ratio) % len(a) == 0):
+                break
+        return (tmp*ratio / len(a)).astype(new_d)
 
 def _round_to_func(dt, td, func, func_name):
     td_units = get_units(td)