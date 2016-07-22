ef522c25c1045f07504e2b5bad626c0aa6ad7910
Steve Marple <s.marple@lancaster.ac.uk>
2016-07-22 21:51:59 +0000

    Enable different plot functions to be set
    'plot', 'step', and 'bar' functions can be selected.

--- auroraplot/bin/plot_mag_data.py
+++ auroraplot/bin/plot_mag_data.py
@@ -101,6 +101,9 @@ parser.add_argument('--offset',
                     default=100,
                     type=float,
                     help='Offset between sites for stack plot (nT)')
+parser.add_argument('--plot-function',
+                    help='Name of matplotlib plot function',
+                    metavar='FUNCTION_NAME')
 parser.add_argument('--qdc-tries', 
                     default=2,
                     type=int,
@@ -289,6 +292,10 @@ for n in range(len(project_list)):
         md = md.mark_missing_data(cadence=2*md.nominal_cadence)
         mdl.append(md)
 
+if args.plot_function:
+    plot_args = dict(plot_func=getattr(plt, args.plot_function))
+else:
+    plot_args = {}
 
 if len(mdl) == 0:
     print('No data to plot')
@@ -297,7 +304,7 @@ if len(mdl) == 0:
 if args.plot_type is None or args.plot_type == 'stack_plot':
     if len(mdl) == 1:
         # No point in using a stackplot for a single site
-        mdl[0].plot()
+        mdl[0].plot(**plot_args)
     else:
         # Create a stackplot.
         ap.magdata.stack_plot(mdl, 
@@ -319,9 +326,9 @@ else:
             k_cadence = dt64.parse_timedelta64(args.k_index_cadence, 's')
             k = ap.auroralactivity.KIndex(magdata=md, magqdc=qdc,
                                           nominal_cadence=k_cadence)
-            k.plot()
+            k.plot(**plot_args)
         else:
-            md.plot()
+            md.plot(**plot_args)
 
 # Override the labelling format for all figures
 for fn in plt.get_fignums():
