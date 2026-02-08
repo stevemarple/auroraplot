3e2ce631951d1ee4050995adf1a71ea2f1ddc86a
Steve Marple <s.marple@systemloco.com>
2026-02-08 18:24:46 +0000

    Support wildcard import for datasets
    Support `from auroraplot.datasets import *` to load all available
    datasets, thus avoiding the need to update scripts when new datasets are
    added.
    
    Add the comment `# noqa` after the import statement if the autopep8
    "Unused import statement" warning is a problem.

---
+++ auroraplot/datasets/__init__.py
@@ -0,0 +1,11 @@
+from pathlib import Path
+
+directory_path = Path(__file__).parent
+
+# Get a list of all .py files in the current directory, except this one.
+modules = [f.stem for f in directory_path.glob('*.py') if f.stem != '__init__']
+
+# Get a list of all directories which contain a __init__.py file, they are packages.
+packages = [f.parent.name for f in directory_path.glob('*/__init__.py')]
+
+__all__ = modules + packages
