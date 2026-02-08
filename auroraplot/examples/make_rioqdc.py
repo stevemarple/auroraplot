e9c163269264f67d7ec15e31cf8085efc215c6bb
Steve Marple <s.marple@systemloco.com>
2026-02-08 21:55:14 +0000

    Add basic riometer data processing
    Add riometer data classes.
    Add definitions for some Lancaster University riometers.

---
+++ auroraplot/examples/make_rioqdc.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python
+
+import auroraplot as ap
+import auroraplot.data
+import auroraplot.dt64tools as dt64
+import auroraplot.datasets.lancsrio
+import auroraplot.riodata
+from auroraplot.riodata.qdc_algorithms import UpperEnvelope
+import logging
+import matplotlib as mpl
+import numpy as np
+import os
+if os.name == 'posix' and ('DISPLAY' not in os.environ or not os.environ['DISPLAY']):
+    mpl.use('Agg')
+
+import matplotlib.pyplot as plt
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
+
+def main():
+    # Set default values for figure
+    mpl.rcParams['figure.facecolor'] = 'w'
+    mpl.rcParams['figure.figsize'] = [8, 6]
+    mpl.rcParams['figure.subplot.bottom'] = 0.1
+    mpl.rcParams['figure.subplot.left'] = 0.12
+    mpl.rcParams['figure.subplot.right'] = 0.925
+    mpl.rcParams['figure.subplot.top'] = 0.85
+    mpl.rcParams['axes.grid'] = True
+    mpl.rcParams['legend.fontsize'] = 'medium'
+    mpl.rcParams['legend.numpoints'] = 1
+
+    t = np.datetime64('2019-08-20')
+    project = "LANCSRIO"
+    site = "KAR1"
+    # site = "KIL1"
+
+    q = dt64.floor(t, np.timedelta64(10, 's'))
+    print(q)
+
+    qdc_t, qdc_load_st, qdc_load_et = auroraplot.riodata.RioData.calculate_qdc_create_times(t, project, site)
+    qdc_load_et = qdc_load_st + np.timedelta64(8, 'D')
+
+    print(f"Making QDC for {auroraplot.format_project_site(project, site)}")
+    print(f"QDC: {qdc_t} ({dt64.fmt_dt64_range(qdc_load_st, qdc_load_et)})")
+    power_data = ap.load_data(project,
+                              site,
+                              data_type='RioPower',
+                              start_time=qdc_load_st,
+                              end_time=qdc_load_et,
+                              channels=['50'],
+                              )
+    print(power_data)
+    algorithm = UpperEnvelope(rows=[1, 2])
+    power_data.plot(channels=['50'])
+
+    qdc = power_data.make_qdc(algorithm)
+    print(qdc)
+    qdc.plot(channels=['50'], title='Fix me')
+    plt.show()
+
+if __name__ == '__main__':
+    main()
---
+++ tests/test_rio_data_qdc_algorithms.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python
+
+from auroraplot.riodata.qdc_algorithms import get_algorithms, get_default_algorithm, QdcAlgorithmBase, UpperEnvelope
+from math import nan
+from numpy import arange, nan, ndarray
+from numpy.testing import assert_array_almost_equal
+from typing import List, Set
+import unittest
+
+
+class TestRioDataQdcAlgorithms(unittest.TestCase):
+    def test_qdc_algorithm(self):
+        """The QdcAlgorithm class should not be instantiated directly.
+
+        Ensure it raise errors
+        """
+        alg = QdcAlgorithmBase()
+        data = [arange(10, dtype=int)]
+        self.assertRaises(NotImplementedError, alg.get_processing)
+        self.assertRaises(NotImplementedError, alg.process, data)
+
+    def test_get_algorithms(self):
+        # We should not expect the algorithms list to be in any particular order. Compare equality using sets.
+        algorithms = set(get_algorithms())
+        expected = {UpperEnvelope}
+        self.assertSetEqual(algorithms, expected)
+        for alg in algorithms:
+            self.assertTrue(issubclass(alg, QdcAlgorithmBase))
+
+    def test_get_default_algorithm(self):
+        default_algorithm = get_default_algorithm()
+        self.assertTrue(issubclass(default_algorithm, QdcAlgorithmBase))
+        self.assertIn(default_algorithm, get_algorithms())
+
+    def test_upper_envelope(self):
+        data = [
+            arange(10, dtype=int) * 1,  # => row 4
+            arange(10, dtype=int) * 2,  # => row 3
+            arange(10, dtype=int) * 4,  # => row 1
+            arange(10, dtype=int) * nan,
+            arange(10, dtype=int) * 3,  # => row 2
+            arange(10, dtype=int) * 5,  # => row 0
+        ]
+
+        ue_row_0 = UpperEnvelope(rows=[0])
+        expected_qdc = arange(10, dtype=int) * 5
+        result: ndarray = ue_row_0.process(data)
+        assert_array_almost_equal(result, expected_qdc)
+
+        ue_row_1_2 = UpperEnvelope(rows=[1, 2])
+        expected_qdc = arange(10, dtype=int) * 3.5
+        result: ndarray = ue_row_1_2.process(data)
+        assert_array_almost_equal(result, expected_qdc)
+
+
+if __name__ == '__main__':
+    unittest.main()
