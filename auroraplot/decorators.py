8cea1bc70553ba9dd3d2f9b7fae75fcfe59d588b
Steve Marple <s.marple@systemloco.com>
2026-02-16 01:57:57 +0000

    Add auroraplot.decorators module
    Add a `deprecated` decorator.

---
+++ auroraplot/decorators.py
@@ -0,0 +1,26 @@
+from typing import Callable, Optional, Union
+from warnings import warn
+
+
+def deprecated(replacement_func: Optional[Union[str, Callable]] = None, message: Optional[str] = None) -> Callable:
+    """Mark a function as deprecated"""
+    def decorator(func: Callable):
+        def wrapper(*args, **kwargs):
+            a = []
+            if replacement_func:
+                if isinstance(replacement_func, str):
+                    replacement = replacement_func
+                else:
+                    replacement = replacement_func.__name__
+                a.append(f"The function '{func.__name__}' is deprecated, use '{replacement}' instead.")
+            else:
+                a.append(f"The function '{func.__name__}' is deprecated.")
+            if message:
+                a.append(message)
+            warn(' '.join(a), DeprecationWarning, stacklevel=2)
+            return func(*args, **kwargs)
+        return wrapper
+    return decorator
+
+
+__all__ = ['deprecated']
